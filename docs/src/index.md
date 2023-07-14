e060903af5d6dadc7c84f2ad7445f3e024e09f97
niclaspopp <niclas-popp@wp-engineering.biz>
2023-07-14 23:06:31 +0200

    Add documentation files

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,22 @@
 # RandomizedDiagonalEstimation.jl
 
-Documentation for RandomizedDiagonalEstimation.jl
+## Overview
+
+RandomizedDiagonalEstimation.jl is a Julia package that implements randomized methods for the estimation of the diagonal of matrices of matrices and matrix functions. For pure matrix diagonal estimation we provide the following algorithms
+* Girard-Hutchinson Estimator
+* Diag++
+* NysDiag++
+* XDiag
+* Full Hutchinson Shifts
+For the estimation of the diagonal of matrix function we combine the Girard-Hutchinson Estimator with the following approximations for ``f(\textbf(A))``
+* Chebyshev interpolants to approximate ``f`` on the interval ``[\lambda_{min},\lambda_{max}]``
+* Minimax polynomials from the Remez algorithm to approximate ``f`` on the interval ``[\lambda_{min},\lambda_{max}]``
+* Arnoldi approximations
+
+The package provides three functions: `EstimateDiagonal`, `EstimateFunctionDiagonal` and `EstimateMoMDiagonal`. The last function incorporates the median of means package into diagonal estimation.
+
+## Estimating the Diagonal of Matrix
+
+```@docs
+EstimateDiagonal
+```
--- src/RandomizedDiagonalEstimation.jl
+++ src/RandomizedDiagonalEstimation.jl
@@ -15,6 +15,7 @@ using Krylov
 using ExponentialUtilities
 using ApproxFun
 using SpecialFunctions
+using Random
 
 ## load files with sub-functions
 include("FunctionDiagonalEstimation.jl")
@@ -25,36 +26,36 @@ include("AdaptiveDiagonalEstimation.jl")
 include("DoublingStrategy.jl")
 
 ## Define main functions
+"""
+    Main function to compute a randomized estimate of the diagonal of a matrix A
 
+    Input:
+        - A: Matrix to estimate the diagonal of
+        - Algorithm: Choose which diagonal estimator is used, options are
+            - :GirardHutchinson
+            - :DiagPP
+            - :NysDiagPP
+            - :XDiag
+            - :GirardHutchinsonShift
+            - :RSVD
+            - :AdaptiveDiagPP
+        - StoppingCriterion: How to terminate, possible options
+            - doubling: Use doubling strategy and terminate when the relative error estimate is below a threshold eps, required parameter
+                - queries_start: number of queries to start with
+                - eps: bound for the relative error estimate
+            - queries: terminate when the maximum number of queries to A is reacher
+                - maxqueries: maximum number of queries to A
+            - adaptive: Using an epsilon delta estimator with :AdaptiveDiagPP
+                - epsdelta: parameters (epsilon,delta) for an epsilon-delta estimator
+                - maxiter: maximum iterations
+        - distribution: Select the distribution from which the random vectors are drawn, inbuilt options are
+            - :Rademacher
+            - :Gaussion
+            - :custom, in this case provide a matrix with the test vectors as columns
+                    - O: matrix with the test vectors as columns
+"""
 function EstimateDiagonal(A::Matrix{Float64},Algorithm::Symbol, StoppingCriterion::Symbol, distribution::Symbol, normalizationParam::Bool=true, parallelizationParam::Bool=false ;maxqueries::Int=0,O=nothing,var_est::Float64=1/eps(),epsilon::Float64=1.0, delta::Float64=1.0,con::Float64=1.0)
-    """
-        Main function to compute a randomized estimate
 
-        Description of the Input:
-            - A: Matrix to estimate the diagonal of
-            - Algorithm: Choose which diagonal estimator is used, options are
-                - :GirardHutchinson
-                - :DiagPP
-                - :NysDiagPP
-                - :XDiag
-                - :GirardHutchinsonShift
-                - :RSVD
-                - :AdaptiveDiagPP
-            - StoppingCriterion: How to terminate, possible options
-                - doubling: Use doubling strategy and terminate when the relative error estimate is below a threshold eps, required parameter
-                    - queries_start: number of queries to start with
-                    - eps: bound for the relative error estimate
-                - queries: terminate when the maximum number of queries to A is reacher
-                    - maxqueries: maximum number of queries to A
-                - adaptive: Using an epsilon delta estimator with :AdaptiveDiagPP
-                    - epsdelta: parameters (epsilon,delta) for an epsilon-delta estimator
-                    - maxiter: maximum iterations
-            - distribution: Select the distribution from which the random vectors are drawn, inbuilt options are
-                - :Rademacher
-                - :Gaussion
-                - :custom, in this case provide a matrix with the test vectors as columns
-                        - O: matrix with the test vectors as columns
-    """
     # check if matrix is square
     (m,n)=size(A)
     if n!=m
